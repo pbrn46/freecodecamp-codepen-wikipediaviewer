2054af17c340c883526bd9146e933ac1fe870b84
Boris Wong <bobobwong@gmail.com>
2017-06-07 19:51:46 -0700

    First commit. Functional.

---
+++ js.js
@@ -0,0 +1,50 @@
+var app = angular.module('wikiViewer', []);
+
+app.factory('resultsService', function () {
+    var data = {
+      r: null
+    };
+
+    function set(key, value) {
+      data[key] = value;
+    }
+
+    return {
+      set: set,
+      data: data
+    };
+  });
+
+
+app.controller('searchList', function resultListControllerController ($scope, resultsService) {
+    $scope.r = resultsService.data;
+  });
+
+app.controller('searchCtrl', function ($http, $sce, $scope, resultsService) {
+    var self = this;
+
+    $scope.searchClick = function () {
+      var url = "https://en.wikipedia.org/w/api.php";
+      var searchValue = $scope.wikiSearchValue;
+      var qs = 'action=opensearch&format=json&search=' + searchValue;
+      url = url + "?" + qs;
+      url = $sce.trustAsResourceUrl(url);
+      searchValue = encodeURIComponent(searchValue);
+      $http.jsonp(url, {
+      })
+        .then(function (response) {
+          resultsService.set('r', response.data);
+          // $('result-list').show();
+          // $('#close-search-button').s
+          $('html, body').animate({
+            scrollTop: $("#search-input").offset().top
+          }, 500);how();
+        });
+    };
+
+    $scope.closeSearchClick = function () {
+      // $('result-list').hide();
+      // $('#close-search-button').hide();
+      resultsService.set('r', {});
+    };
+  });
